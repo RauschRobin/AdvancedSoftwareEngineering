24741d244f07ae1b354305da83e75cdee084dbdf
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-24 20:47:15 +0200

    new requirements txt

--- core/shared/deutschebahn/deutschebahn.py
+++ core/shared/deutschebahn/deutschebahn.py
@@ -1,4 +1,4 @@
-#from ...communication.voice_output import VoiceOutput
+from ...communication.voice_output import VoiceOutput
 # https://pypi.org/project/deutsche-bahn-api/ --> Wenn man das benutzen möchte
 
 
@@ -48,8 +48,8 @@ class FilterByDestination(TimetableDecorator):
     
 
 class DeutscheBahn:
-    def __init__(self): #, voice_output:VoiceOutput
-        self.voice_output = None # <- voice_output
+    def __init__(self, voice_output:VoiceOutput):
+        self.voice_output = voice_output
 
     def getConnection(self):
         self.voice_output.add_message("Bietigheim-Bissingen|Stuttgart Hbf, S5, 12:37")
