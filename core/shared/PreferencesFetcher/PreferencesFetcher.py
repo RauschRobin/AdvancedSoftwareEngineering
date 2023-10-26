8dda8536c77ef148674526980e79765c7c00c56b
RauschRobin <robin.rausch@kabelbw.de>
2023-10-26 11:22:39 +0200

    fix test failure

--- core/shared/PreferencesFetcher/PreferencesFetcher.py
+++ core/shared/PreferencesFetcher/PreferencesFetcher.py
@@ -3,6 +3,14 @@ import yaml
 class PreferencesFetcher:
     @staticmethod
     def fetch(key, filepath="preferences.yaml"):
-        with open(filepath, 'r') as file:
-            preferences = yaml.safe_load(file)
-            return preferences['user_preferences'][key]
+        try:
+            with open(filepath, 'r') as file:
+                preferences = yaml.safe_load(file)
+                if 'user_preferences' in preferences and key in preferences['user_preferences']:
+                    return preferences['user_preferences'][key]
+                else:
+                    raise KeyError(f"Key '{key}' not found in preferences file")
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Preferences file '{filepath}' not found")
+        except yaml.YAMLError as e:
+            raise ValueError(f"Error parsing preferences file: {e}")
--- core/shared/PreferencesFetcher/tests/test_PreferencesFetcher.py
+++ core/shared/PreferencesFetcher/tests/test_PreferencesFetcher.py
@@ -1,12 +1,17 @@
-import unittest
 from unittest.mock import mock_open, patch
-from ..PreferencesFetcher import PreferencesFetcher
 import yaml
+from ..PreferencesFetcher import PreferencesFetcher
+import pytest
 
-class TestPreferencesFetcher(unittest.TestCase):
-    def test_fetch(self):
-        preferences = {'key1': 'value1', 'key2': 'value2'}
-        m = mock_open(read_data=yaml.dump(preferences))
-        with patch('builtins.open', m):
+class TestPreferencesFetcher:
+    def test_fetch_existing_key(self):
+        preferences = {'user_preferences': {'key1': 'value1', 'key2': 'value2'}}
+        with patch('builtins.open', mock_open(read_data=yaml.dump(preferences))):
             result = PreferencesFetcher.fetch('key1')
-            self.assertEqual(result, 'value1')
+            assert result == 'value1'
+
+    def test_fetch_nonexistent_key(self):
+        preferences = {'user_preferences': {'key1': 'value1', 'key2': 'value2'}}
+        with patch('builtins.open', mock_open(read_data=yaml.dump(preferences))):
+            with pytest.raises(KeyError):
+                PreferencesFetcher.fetch('nonexistent_key')
