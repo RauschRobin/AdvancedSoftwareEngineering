80c8ba2a051b419b7d8ca5f24b188758e100b37c
Florian <florian.maslowski@gmail.com>
2023-10-30 17:24:12 +0100

    testen obs jetzt in github klappt

--- core/shared/rapla/test_rapla/test_rapla.py
+++ core/shared/rapla/test_rapla/test_rapla.py
@@ -1,19 +1,22 @@
 import pytest
 from unittest.mock import Mock, patch
-from rapla import Rapla  # Replace 'your_module' with the actual module name
+from rapla.rapla import Rapla
+import datetime
 
 @pytest.fixture
 def rapla_instance():
     return Rapla()
 
 def test_get_current_calendar_week(rapla_instance):
-    with patch('your_module.datetime') as mock_datetime:
+    with patch('rapla.datetime') as mock_datetime:
         mock_datetime.now.return_value = Mock(isocalendar=lambda: (2023, 43))
         result = rapla_instance.get_current_calendar_week()
+        print(result)
         assert result == 43
 
 def test_get_calendar_week(rapla_instance):
     result = rapla_instance.get_calendar_week(2023, 10, 26)
+    print(result)
     assert result == 43  # Assuming October 26, 2023, is in week 43
 
 def test_get_week_day(rapla_instance):
@@ -22,10 +25,12 @@ def test_get_week_day(rapla_instance):
 
 def test_get_date_from_week(rapla_instance):
     result = rapla_instance.get_date_from_week(2023, 43, 3)
+    print(result)
     assert result == datetime.date(2023, 10, 26)
 
 def test_get_week_data(rapla_instance):
-    with patch('your_module.requests.get') as mock_get:
+    with patch('rapla.requests.get') as mock_get:
         mock_get.return_value.text = "HTML content here"
         result = rapla_instance.get_week_data(43, 2023)
+        print(result)
         assert isinstance(result, str)  # Check if it's a JSON string
