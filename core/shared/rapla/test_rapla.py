5c3e373241b28fe782625b7e1a5cdd0cca9b1f12
Florian <florian.maslowski@gmail.com>
2023-10-30 17:09:10 +0100

    test changed to pytest

--- core/shared/rapla/test_rapla.py
+++ core/shared/rapla/test_rapla.py
@@ -1,38 +1,31 @@
-import unittest
-import datetime
-import dateutil.relativedelta as rd
-from rapla import Rapla # TODO: relativen Pfad from 'rapla' finden und Klasse Rapla importieren
+import pytest
+from unittest.mock import Mock, patch
+from rapla import Rapla  # Replace 'your_module' with the actual module name
 
-class TestRapla(unittest.TestCase):
-    def setUp(self):
-        self.rapla = Rapla()
+@pytest.fixture
+def rapla_instance():
+    return Rapla()
 
-    def test_get_current_calendar_week(self):
-        result = self.rapla.get_current_calendar_week()
-        expected = datetime.datetime.now().isocalendar()[1]
-        self.assertEqual(result, expected, "Fehler in get_current_calendar_week Methode")
+def test_get_current_calendar_week(rapla_instance):
+    with patch('your_module.datetime') as mock_datetime:
+        mock_datetime.now.return_value = Mock(isocalendar=lambda: (2023, 43))
+        result = rapla_instance.get_current_calendar_week()
+        assert result == 43
 
-    def test_get_calendar_week(self):
-        result = self.rapla.get_calendar_week(2023, 10, 26)
-        expected = datetime.date(2023, 10, 26).isocalendar()[1]
-        self.assertEqual(result, expected, "Fehler in get_calendar_week Methode")
+def test_get_calendar_week(rapla_instance):
+    result = rapla_instance.get_calendar_week(2023, 10, 26)
+    assert result == 43  # Assuming October 26, 2023, is in week 43
 
-    def test_get_week_day(self):
-        days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-        for i in range(7):
-            result = self.rapla.get_week_day(i)
-            expected = days[i]
-            self.assertEqual(result, expected, f"Fehler in get_week_day Methode für {expected}")
+def test_get_week_day(rapla_instance):
+    assert rapla_instance.get_week_day(0) == "monday"
+    assert rapla_instance.get_week_day(1) == "tuesday"
 
-    def test_get_date_from_week(self):
-        result = self.rapla.get_date_from_week(2023, 43, 3)
-        first_day_of_year = datetime.datetime(2023, 1, 1)
-        if first_day_of_year.weekday() > 3:
-            first_monday_of_year = first_day_of_year + rd.relativedelta(days=(7 - first_day_of_year.weekday() + 1))
-        else:
-            first_monday_of_year = first_day_of_year - rd.relativedelta(days=(first_day_of_year.weekday() - 1))
-        expected = (first_monday_of_year + rd.relativedelta(days=((43 - 1) * 7 + 3))).date()
-        self.assertEqual(result, expected, "Fehler in get_date_from_week Methode")
+def test_get_date_from_week(rapla_instance):
+    result = rapla_instance.get_date_from_week(2023, 43, 3)
+    assert result == datetime.date(2023, 10, 26)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_get_week_data(rapla_instance):
+    with patch('your_module.requests.get') as mock_get:
+        mock_get.return_value.text = "HTML content here"
+        result = rapla_instance.get_week_data(43, 2023)
+        assert isinstance(result, str)  # Check if it's a JSON string
