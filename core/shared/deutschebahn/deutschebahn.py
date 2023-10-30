9c16109cc709140a97bb87a8ccd141480dd257c4
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-30 11:32:08 +0100

    comments now pep8 standard

--- core/shared/deutschebahn/deutschebahn.py
+++ core/shared/deutschebahn/deutschebahn.py
@@ -14,18 +14,22 @@ class DeutscheBahn:
     def getConnection(self):
         self.voice_output.add_message("Bietigheim-Bissingen|Stuttgart Hbf, S5, 12:37")
 
-    # Description: Returns one station that fits to the inputtext
-    # Input: E.g. station_name="Stuttgart Hbf (tief)"
+    """Returns one station that fits to the inputtext
+    
+    Input: E.g. station_name="Stuttgart Hbf (tief)"
     # Return: E.g. {'name': 'Stuttgart Hbf (tief)', 'eva': '8098096', 'db': 'true'}
+    """
     def getStationDetailByStationname(self, station_name: str):
         station_request = StationRequest(station_name)
         station = station_request.execute()
 
         return station
 
-    # Description: Returns Timetable (all trains) for a given line, stationid and date and hour
-    # Input: E.g. line="4" station_id="8098096", date="231025", hour="10"
-    # Output: E.g. { 'station': 'NAME', 'timetable': {[ Object ]}}
+    """ Return JSON data for a given line, stationid and date and hour.
+
+    Input: E.g. line="4" station_id="8098096", date="231025", hour="10"
+    Output: E.g. { 'station': 'NAME', 'timetable': {[ Object ]}}
+    """
     def getTimetableByLineStationidDateHour(self, line: str, station_id: str, date: str, hour: str):
         timetable_request = TimetableRequest(station_id, date, hour) 
         data = timetable_request.execute()
@@ -36,9 +40,11 @@ class DeutscheBahn:
 
         return filterd_by_line.data()
 
-    # Description: Returns Timetable (all trains) for a given destination, stationid and date and hour.
-    # Input: E.g. destination="Bietigheim-Bissingen" station_id="8098096", date="231025", hour="10"
-    # Output: E.g. {'station': 'NAME', 'timetable': {[ Object ]}}
+    """Returns Timetable (all trains) for a given destination, stationid and date and hour.
+     
+    Input: E.g. destination="Bietigheim-Bissingen" station_id="8098096", date="231025", hour="10"
+    Output: E.g. {'station': 'NAME', 'timetable': {[ Object ]}}
+    """
     def getTimetableByDestinationStationidDateHour(self, destination: str, station_id: str, date: str, hour: str):
         timetable_request = TimetableRequest(station_id, date, hour)
         data = timetable_request.execute()
