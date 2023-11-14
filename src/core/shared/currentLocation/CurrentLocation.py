60d8316e5ac5991270f29f327334141c9817118b
unknown <inf21043@lehre.dhbw-stuttgart.de>
2023-11-14 11:15:47 +0100

    modified code, added docstrings and README.md

--- src/core/shared/currentLocation/CurrentLocation.py
+++ src/core/shared/currentLocation/CurrentLocation.py
@@ -9,15 +9,13 @@ class CurrentLocation:
         pass
 
     def get_location(self) -> str:
+        """Returns the location based on the current time
+        
+        Returns: String
+        """
         t = time.localtime()
         current_time = time.strftime("%H", t)
         if (int(current_time) > 7 or int(current_time) < 16):
             return "Uni"
         else:
             return "Home"
-
-
-ort = CurrentLocation()
-ort.get_location()
-print(type(ort))
-#print(isinstance(ort, CurrentLocation))
