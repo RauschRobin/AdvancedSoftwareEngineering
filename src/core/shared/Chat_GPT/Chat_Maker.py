e59797d8d9f45a690aa83c4bcbd10e0e69460f8a
Florian <florian.maslowski@gmail.com>
2023-11-14 21:22:29 +0100

    Unnötigen Kommentar entfernt

--- src/core/shared/Chat_GPT/Chat_Maker.py
+++ src/core/shared/Chat_GPT/Chat_Maker.py
@@ -16,19 +16,3 @@ class ChatMaker:
 if __name__ == "__main__":
     myObj = ChatMaker('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')
     respones = myObj.get_response("Write a tagline for an ice cream shop.")
-
-
-# from openai import OpenAI
-# client = OpenAI()
-
-# client.api_key = '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'
-
-# completion = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a helpful assistant."},
-#     {"role": "user", "content": "Hello!"}
-#   ]
-# )
-
-# print(completion.choices[0].message)
