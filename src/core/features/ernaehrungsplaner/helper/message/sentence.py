bcd205c1957daaeba133cd837f3cfc74a66977ec
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-11-18 21:24:27 +0100

    removed long docstring comments

--- src/core/features/ernaehrungsplaner/helper/message/sentence.py
+++ src/core/features/ernaehrungsplaner/helper/message/sentence.py
@@ -2,15 +2,6 @@ from typing import Any
 
 
 class Sentence1():
-    """
-    It makes sense to use the Builder pattern only when your products are quite
-    complex and require extensive configuration.
-
-    Unlike in other creational patterns, different concrete builders can produce
-    unrelated products. In other words, results of various builders may not
-    always follow the same interface.
-    """
-
     def __init__(self) -> None:
         self.sentences = []
 
