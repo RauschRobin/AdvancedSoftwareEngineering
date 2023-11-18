32b61dd416b2f8721945f7e2ab6eccf056a4c90d
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-11-18 13:06:17 +0100

    added yelp helper tests

--- src/tests/test_shared_yelp_helper.py
+++ src/tests/test_shared_yelp_helper.py
@@ -10,3 +10,21 @@ def test_add_param_new_key():
 
     assert "key1" in builder._params
     assert builder._params["key1"] == "value1"
+
+
+def test_add_param_update_key():
+    builder = YelpSearchRequestBuilder()
+    builder.add_param("key1", "value1")
+    builder.add_param("key1", "new_value")
+
+    assert builder._params["key1"] == "new_value"
+
+
+def test_add_param_multiple_keys():
+    builder = YelpSearchRequestBuilder()
+    builder.add_param("key1", "value1")
+    builder.add_param("key2", "value2")
+
+    assert "key1" in builder._params and "key2" in builder._params
+    assert builder._params["key1"] == "value1"
+    assert builder._params["key2"] == "value2"
