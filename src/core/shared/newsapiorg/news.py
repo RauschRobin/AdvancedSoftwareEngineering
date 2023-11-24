5c7fa10c2da6654f3fc89bf96c9808e720e1b181
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-11-24 19:30:53 +0100

    added dotenv

--- src/core/shared/newsapiorg/news.py
+++ src/core/shared/newsapiorg/news.py
@@ -1,5 +1,8 @@
+import os
+from dotenv import load_dotenv
 from newsapi import NewsApiClient
 from ..YamlFetcher.YamlFetcher import YamlFetcher
+
 
 class NewsAPI:
     '''
@@ -13,10 +16,11 @@ class NewsAPI:
         Parameters: api_key - string (optional)
         Returns: None
         '''
+        load_dotenv()
         if override_api_key:
             self.newsapi = NewsApiClient(api_key=override_api_key)
         else:
-            self.newsapi = NewsApiClient(YamlFetcher.fetch("newsapi", "API_Keys.yaml"))
+            self.newsapi = NewsApiClient(api_key=os.getenv('NESAPI_SECRET'))
 
     def get_top_headlines(self, search_keyword=None, sources=None, category=None, language=None, country=None):
         '''
