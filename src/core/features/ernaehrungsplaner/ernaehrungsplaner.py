129c2e9b6f7a763ea3150fcddf119373f5481f35
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-11-24 20:11:40 +0100

    updated github workflow

--- src/core/features/ernaehrungsplaner/ernaehrungsplaner.py
+++ src/core/features/ernaehrungsplaner/ernaehrungsplaner.py
@@ -92,6 +92,10 @@ class Ernaehrungsplaner:
         '''
         now = datetime.datetime.now()
 
+        chat = ChatGpt()
+        test = chat.get_response("Hallo")
+        print(test)
+
         # TODO
         # Basic Lunchbreak (12 am)
         # - Calculate the lunchbreak time via rapla // if no then tell the user 30 minutes
