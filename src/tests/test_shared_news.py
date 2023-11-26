9ba2890f0aee5942c42eafef894ace65e4ae0c23
RobinRaus <65980863+RobinRaus@users.noreply.github.com>
2023-11-26 15:28:39 +0100

    please run tests

--- src/tests/test_shared_news.py
+++ src/tests/test_shared_news.py
@@ -5,14 +5,17 @@ import pytest
 def news_api():
     return NewsAPI()    #does not work?
 
+# Tests get_top_headlines()
 def test_get_top_headlines(news_api):
     response = news_api.get_top_headlines(search_keyword='bitcoin')
     assert response['status'] == 'ok'
 
+# Tests get_everything()
 def test_get_everything(news_api):
     response = news_api.get_everything(search_keyword='bitcoin')
     assert response['status'] == 'ok'
 
+# Tests get_sources()
 def test_get_sources(news_api):
     response = news_api.get_sources()
     assert response['status'] == 'ok'
