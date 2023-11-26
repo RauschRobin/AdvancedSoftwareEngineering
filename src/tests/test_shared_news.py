e1c1d2cd1223cb8060d917de3a976b94ef328f04
RobinRaus <65980863+RobinRaus@users.noreply.github.com>
2023-11-26 16:49:05 +0100

    create new newsapi.org tests that will work

--- src/tests/test_shared_news.py
+++ src/tests/test_shared_news.py
@@ -1,20 +1,28 @@
-from ..core.shared.newsapiorg.news import NewsAPI
 import pytest
-'''
-# pytest fixture
+from unittest.mock import Mock
+from ..core.shared.newsapiorg.news import NewsAPI
+
 @pytest.fixture
-def news_api():
-    return NewsAPI()
+def newsapi_instance():
+    # Mocking the NewsApiClient so that we don't make actual API calls during testing
+    newsapi = NewsAPI()
+    newsapi.newsapi = Mock()
+    return newsapi
 
-def test_get_top_headlines(news_api):
-    response = news_api.get_top_headlines(search_keyword='bitcoin')
-    assert response['status'] == 'ok'
+def test_get_top_headlines(newsapi_instance):
+    # Test when search_keyword is provided
+    result = newsapi_instance.get_top_headlines(search_keyword='bitcoin')
+    newsapi_instance.newsapi.get_top_headlines.assert_called_once_with(q='bitcoin', sources=None, category=None, language=None, country=None)
 
-def test_get_everything(news_api):
-    response = news_api.get_everything(search_keyword='bitcoin')
-    assert response['status'] == 'ok'
+def test_get_everything(newsapi_instance):
+    # Test when search_keyword is not provided
+    result = newsapi_instance.get_everything(search_keyword=None)
+    assert result is None
 
-def test_get_sources(news_api):
-    response = news_api.get_sources()
-    assert response['status'] == 'ok'
-'''
+    # Test when search_keyword is provided
+    result = newsapi_instance.get_everything(search_keyword='bitcoin')
+    newsapi_instance.newsapi.get_everything.assert_called_once_with(q='bitcoin', sources=None, domains=None, from_param=None, to=None, language=None, sort_by=None, page=None)
+
+def test_get_sources(newsapi_instance):
+    result = newsapi_instance.get_sources()
+    newsapi_instance.newsapi.get_sources.assert_called_once()
