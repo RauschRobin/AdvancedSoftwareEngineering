03a25eacb8ff79c68e3bf901512bd6cbc91fc774
RobinRaus <65980863+RobinRaus@users.noreply.github.com>
2023-11-26 15:29:53 +0100

    correct loading of github secrets

--- src/tests/test_shared_news.py
+++ src/tests/test_shared_news.py
@@ -3,19 +3,16 @@ import pytest
 
 @pytest.fixture
 def news_api():
-    return NewsAPI()    #does not work?
+    return NewsAPI()
 
-# Tests get_top_headlines()
 def test_get_top_headlines(news_api):
     response = news_api.get_top_headlines(search_keyword='bitcoin')
     assert response['status'] == 'ok'
 
-# Tests get_everything()
 def test_get_everything(news_api):
     response = news_api.get_everything(search_keyword='bitcoin')
     assert response['status'] == 'ok'
 
-# Tests get_sources()
 def test_get_sources(news_api):
     response = news_api.get_sources()
     assert response['status'] == 'ok'
